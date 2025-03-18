fa1e19970c129f53146378405f1777e18964d9ad
Nataliia <nperepeliak7@gmail.com>
2025-03-18 22:38:23 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -1,1 +1,22 @@
-# write your code here
+import os
+
+
+def copy_file(command: str) -> None:
+    parts = command.split()
+
+    if len(parts) != 3 or parts[0] != "cp":
+        return
+
+    src_file = parts[1]
+    dest_file = parts[2]
+
+    if src_file == dest_file:
+        return
+
+    if not os.path.exists(src_file):
+        print(f"Error: '{src_file}' does not exist.")
+        return
+
+    with open(src_file, "r") as file_in, open(dest_file, "w") as file_out:
+        content = file_in.read()
+        file_out.write(content)
